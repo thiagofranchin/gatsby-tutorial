111f82d7b9e20ac7d6c6f9afbae178545eb3c71a
Thiago Franchin <thiago.franchin@gmail.com>
2020-01-13 12:04:12 -0300

    React Bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,4 @@
 import './src/styles/styles.scss'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
--- src/components/CardV2/index.jsx
+++ src/components/CardV2/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import { Dropdown } from 'react-bootstrap'
 
 const CardV2 = (props) => {
   return(
@@ -6,7 +7,17 @@ const CardV2 = (props) => {
       <div className="card-body">
         <h5 className="card-title">{props.titleCard}</h5>
         <p className="card-text text-gatsby">{props.textCard}</p>
-        <a href="#" className={`btn btn-${props.colorButton}`}>Visitar</a>
+        <a href="./" className={`btn btn-${props.colorButton}`}>Visitar</a>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown button
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
       </div>
     </div>
   )
